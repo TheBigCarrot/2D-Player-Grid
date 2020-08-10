ecbe32287591fc5102c6689a08e9369e383a5254
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2020-08-09 18:32:47 -0700

    2D Player Grid v2.1
    Huge Change. I changed it from vectors to a 2d array. Works MUCH better vectors and way easier to code with. I also changed the way it changed your location on the graph when you press a button, since the previous method with vectors wouldn't allow barriers in the future.
    
    Also, this is v2.1 because I forgot to add v2.0. After I made the change from vectors to a 2d array i also added messages to let you know how to exit the game and if you're at a border.

---
+++ Test Grid 2.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+void Alert(int alert)
+{
+	switch (alert)
+	{
+	case 0:
+		break;
+
+	case 1:
+		cout << "Press 'P' at any time to quit playing.\n";
+		break;
+
+	case 2:
+		cout << "Unable to move to the right.\n";
+		break;
+
+	case 3:
+		cout << "Unable to move to the left.\n";
+		break;
+
+	case 4:
+		cout << "Unable to move down.\n";
+		break;
+
+	case 5:
+		cout << "Unable to move up.\n";
+		break;
+
+	default:
+		break;
+	}
+}
+
+void ShowMap(int plane[10][10], int x, int y, int alert)
+{
+	system("cls");  //This is windows only. This will be fixed later.
+	cout << "Map Location:\n";
+	cout << "X locaiton = " << x + 1<< endl;
+	cout << "Y location = " << y + 1<< endl;
+	for (int r = 0; r < 10; r++)
+	{
+		for (int c = 0; c < 10; c++)
+		{
+			cout << plane[c][r] << " ";
+		}
+		cout << endl;
+	}
+	Alert(alert);
+}
+
+int main()
+{
+	int plane[10][10] = {
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+	};
+	int x = 0; //x goes from 0 to 9 since arrays start at 0. it will make the code easier.
+	int y = 0; //y goes from 0 to 9 since arrays start at 0. it will make the code easier.
+	char input;
+	int alert;
+
+	ShowMap(plane, x, y, 1);
+
+
+	while (true)
+	{
+		alert = 0;
+
+		input = _getch();  //I think this is windows only. This will be fixed later.
+		if (input == 'p')
+		{
+			break;
+		}
+		if (input == 'd')
+		{
+			if (x == 9)
+			{
+				alert = 2;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x + 1][y] = 1;
+				x++;
+			}
+		}
+		if (input == 'a')
+		{
+			if (x == 0)
+			{
+				alert = 3;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x - 1][y] = 1;
+				x--;
+			}
+		}
+		if (input == 's')
+		{
+			if (y == 9)
+			{
+				alert = 4;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x][y + 1] = 1;
+				y++;
+			}
+		}
+		if (input == 'w')
+		{
+			if (y == 0)
+			{
+				alert = 5;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x][y - 1] = 1;
+				y--;
+			}
+		}
+
+		ShowMap(plane, x, y, alert);
+
+	}
+}
