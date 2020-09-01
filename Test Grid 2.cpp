29ae2594171b2bb2c0c9fb377d41d73778ab042c
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2020-09-01 14:34:12 -0700

    Add files via upload

--- Test Grid 2.cpp
+++ Test Grid 2.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void Alert(int alert)
+void Alert(int alert)  //this is how the game will send a message to the player. (If the player cant move, found an item, lost health, etc.)
 {
 	switch (alert)
 	{
@@ -14,7 +14,7 @@ void Alert(int alert)
 		break;
 
 	case 1:
-		cout << "Press 'P' at any time to quit playing.\n";
+		cout << "Press 'P' at any time to5 quit playing.\n";
 		break;
 
 	case 2:
@@ -44,11 +44,28 @@ void ShowMap(int plane[16][16], int x, int y, int alert, int x_max, int y_max)
 	cout << "Map Location:\n";
 	cout << "X locaiton = " << x + 1<< endl;
 	cout << "Y location = " << y + 1<< endl;
-	for (int r = 0; r < y_max; r++)
+	for (int c = 0; c < x_max; c++)
 	{
-		for (int c = 0; c < x_max; c++)
+		for (int r = 0; r < y_max; r++)
 		{
-			cout << plane[c][r] << " ";
+			switch (plane[c][r])
+			{
+			case 0:
+				cout << '.' << " ";
+				break;
+
+			case 1:
+				cout << '@' << " ";
+				break;
+
+			case 2:
+				cout << '#' << " ";
+				break;
+
+			default:
+				break;
+			}
+
 		}
 		cout << endl;
 	}
@@ -57,13 +74,28 @@ void ShowMap(int plane[16][16], int x, int y, int alert, int x_max, int y_max)
 
 int main()
 {
-	int plane[16][16] = {
+	int plane[16][16] = { //when changing map size, change dimensions here.
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 2, 2, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
+		{ 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
 	};
 	int x = 0; //x goes from 0 to 9 since arrays start at 0. it will make the code easier.
 	int y = 0; //y goes from 0 to 9 since arrays start at 0. it will make the code easier.
-	const int x_max = 16;
-	const int y_max = 16;
+	const int x_max = 16; //when changing map size, change dimension size here.
+	const int y_max = 16; //when changing map size, change dimension size here.
 	char input;
 	int alert;
 
@@ -81,33 +113,7 @@ int main()
 		}
 		if (input == 'd')
 		{
-			if (x == x_max - 1)
-			{
-				alert = 2;
-			}
-			else
-			{
-				plane[x][y] = 0;
-				plane[x + 1][y] = 1;
-				x++;
-			}
-		}
-		if (input == 'a')
-		{
-			if (x == 0)
-			{
-				alert = 3;
-			}
-			else
-			{
-				plane[x][y] = 0;
-				plane[x - 1][y] = 1;
-				x--;
-			}
-		}
-		if (input == 's')
-		{
-			if (y == y_max - 1)
+			if (y == y_max - 1 || plane[x][y + 1] == 2)
 			{
 				alert = 4;
 			}
@@ -118,9 +124,9 @@ int main()
 				y++;
 			}
 		}
-		if (input == 'w')
+		if (input == 'a')
 		{
-			if (y == 0)
+			if (y == 0 || plane[x][y - 1] == 2)
 			{
 				alert = 5;
 			}
@@ -129,6 +135,32 @@ int main()
 				plane[x][y] = 0;
 				plane[x][y - 1] = 1;
 				y--;
+			}
+		}
+		if (input == 's')
+		{
+			if (x == x_max - 1 || plane[x + 1][y] == 2)
+			{
+				alert = 2;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x + 1][y] = 1;
+				x++;
+			}
+		}
+		if (input == 'w')
+		{
+			if (x == 0 || plane[x - 1][y] == 2)
+			{
+				alert = 3;
+			}
+			else
+			{
+				plane[x][y] = 0;
+				plane[x - 1][y] = 1;
+				x--;
 			}
 		}
 
