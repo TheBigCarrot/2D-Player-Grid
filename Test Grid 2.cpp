fbc9990f3326c2bbeb39045e6c4933c37fc22b48
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2020-09-02 18:32:26 -0700

    Test Grid v2.3
    I support for multiple levels by changing the 2 dimensional arrays to a 3 dimensional arrays. You move between levels using the "teleporters". (I used a '0' to indicate a teleporter.)

--- Test Grid 2.cpp
+++ Test Grid 2.cpp
@@ -38,17 +38,18 @@ void Alert(int alert)  //this is how the game will send a message to the player.
 	}
 }
 
-void ShowMap(int plane[16][16], int x, int y, int alert, int x_max, int y_max)
+void ShowMap(int plane[4][16][16], int x, int y, int level, int alert, int x_max, int y_max)
 {
 	system("cls");  //This is windows only. This will be fixed later.
 	cout << "Map Location:\n";
 	cout << "X locaiton = " << x + 1<< endl;
 	cout << "Y location = " << y + 1<< endl;
+	cout << "Level Location: " << level + 1 << endl;
 	for (int c = 0; c < x_max; c++)
 	{
 		for (int r = 0; r < y_max; r++)
 		{
-			switch (plane[c][r])
+			switch (plane[level][c][r])
 			{
 			case 0:
 				cout << '.' << " ";
@@ -60,6 +61,14 @@ void ShowMap(int plane[16][16], int x, int y, int alert, int x_max, int y_max)
 
 			case 2:
 				cout << '#' << " ";
+				break;
+
+			case 3:
+				cout << '0' << " ";
+				break;
+
+			case 4:
+				cout << '0' << " ";
 				break;
 
 			default:
@@ -74,7 +83,7 @@ void ShowMap(int plane[16][16], int x, int y, int alert, int x_max, int y_max)
 
 int main()
 {
-	int plane[16][16] = { //when changing map size, change dimensions here.
+	int plane[4][16][16] = { { //when changing map size, change dimensions here.
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
 		{ 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, },
@@ -82,7 +91,7 @@ int main()
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, },
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, },
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 2, 2, 2, },
+		{ 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, },
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
 		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, },
@@ -90,16 +99,72 @@ int main()
 		{ 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, },
+	}, {
+		{ 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-	};
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, },
+	}, {
+		{ 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, },
+	}, {
+		{ 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+	} };
 	int x = 0; //x goes from 0 to 9 since arrays start at 0. it will make the code easier.
 	int y = 0; //y goes from 0 to 9 since arrays start at 0. it will make the code easier.
+	int level = 0; //this is a pointer for which 2d plane the charcter is currently moving on
 	const int x_max = 16; //when changing map size, change dimension size here.
 	const int y_max = 16; //when changing map size, change dimension size here.
 	char input;
 	int alert;
 
-	ShowMap(plane, x, y, 1, x_max, y_max);
+	/*
+	NOTE TO SELF: ARRAYS START AT 0
+	*/
+
+	ShowMap(plane, x, y, level, 1, x_max, y_max);
 
 
 	while (true)
@@ -113,58 +178,118 @@ int main()
 		}
 		if (input == 'd')
 		{
-			if (y == y_max - 1 || plane[x][y + 1] == 2)
+			if (x == x_max - 1 || plane[level][y][x + 1] == 2)
 			{
 				alert = 4;
 			}
+			else if (plane[level][y][x + 1] == 4)
+			{
+				plane[level][y][x] = 0;
+				y = 1;
+				plane[level + 1][y][x + 1] = 1;
+				x++;
+				level++;
+			}
+			else if (plane[level][y][x + 1] == 3)
+			{
+				plane[level][y][x] = 0;
+				y = 14;
+				plane[level - 1][y][x + 1] = 1;
+				x++;
+				level--;
+			}
 			else
 			{
-				plane[x][y] = 0;
-				plane[x][y + 1] = 1;
-				y++;
+				plane[level][y][x] = 0;
+				plane[level][y][x + 1] = 1;
+				x++;
 			}
 		}
 		if (input == 'a')
 		{
-			if (y == 0 || plane[x][y - 1] == 2)
+			if (x == 0 || plane[level][y][x - 1] == 2)
 			{
 				alert = 5;
 			}
+			else if (plane[level][y][x - 1] == 4)
+			{
+				plane[level][y][x] = 0;
+				y = 1;
+				plane[level + 1][y][x - 1] = 1;
+				x--;
+				level++;
+			}
+			else if (plane[level][y][x - 1] == 3)
+			{
+				plane[level][y][x] = 0;
+				y = 14;
+				plane[level - 1][y][x - 1] = 1;
+				x--;
+				level--;
+			}
 			else
 			{
-				plane[x][y] = 0;
-				plane[x][y - 1] = 1;
-				y--;
+				plane[level][y][x] = 0;
+				plane[level][y][x - 1] = 1;
+				x--;
 			}
 		}
 		if (input == 's')
 		{
-			if (x == x_max - 1 || plane[x + 1][y] == 2)
+			if (y == y_max - 1 || plane[level][y + 1][x] == 2)
 			{
 				alert = 2;
 			}
+			else if (plane[level][y + 1][x] == 4)
+			{
+				plane[level][y][x] = 0;
+				y = 1;
+				plane[level + 1][y][x] = 1;
+				level++;
+			}
+			else if (plane[level][y + 1][x] == 3)
+			{
+				plane[level][y][x] = 0;
+				y = 14;
+				plane[level - 1][y][x] = 1;
+				level--;
+			}
 			else
 			{
-				plane[x][y] = 0;
-				plane[x + 1][y] = 1;
-				x++;
+				plane[level][y][x] = 0;
+				plane[level][y + 1][x] = 1;
+				y++;
 			}
 		}
 		if (input == 'w')
 		{
-			if (x == 0 || plane[x - 1][y] == 2)
+			if (y == 0 || plane[level][y - 1][x] == 2)
 			{
 				alert = 3;
 			}
+			else if (plane[level][y - 1][x] == 4)
+			{
+				plane[level][y][x] = 0;
+				y = 1;
+				plane[level][y][x] = 1;
+				level++;
+			}
+			else if (plane[level][y - 1][x] == 3)
+			{
+				plane[level][y][x] = 0;
+				y = 14;
+				plane[level - 1][y][x] = 1;
+				level--;
+			}
 			else
 			{
-				plane[x][y] = 0;
-				plane[x - 1][y] = 1;
-				x--;
+				plane[level][y][x] = 0;
+				plane[level][y - 1][x] = 1;
+				y--;
 			}
 		}
 
-		ShowMap(plane, x, y, alert, x_max, y_max);
+		ShowMap(plane, x, y, level, alert, x_max, y_max);
 
 	}
 }
